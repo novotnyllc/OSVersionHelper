34ef8533742d9b2b5b2e906afb75ce1be6f37ca3
Oren Novotny <oren@novotny.org>
2019-01-02 17:52:41 -0500

    Initial checkin

---
+++ OSVersionHelper/WindowsVersions.cs
@@ -0,0 +1,18 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace OSVersionHelper
+{
+    public enum WindowsVersions
+    {
+        Win7,
+        Server2008R2,   // 6.1
+        Win8,
+        Server2012,     // 6.2
+        Win81,
+        Server2012R2,   // 6.3
+        Win10,
+        Server2016,     // 10.0
+        Server2019 
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# OSVersionHelper
+
+A helper class to returned detailed information on the different Windows versions
